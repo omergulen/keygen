2210e1c4caf074c7f0ef733253145a52c659ef8a
Mark Topper <mail@webman.io>
2015-09-22 18:53:15 +0200

    Added privacy link

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 				</div>
 				
 				<footer style="margin-bottom: 20px;">
-					Created by <a href="https://www.marktopper.com">Mark Topper</a> at <a href="https://www.webman.io">webman.io</a>. Source available at <a href="https://github.com/marktopper/keygen.io">Github</a>.
+					Created by <a href="https://www.marktopper.com">Mark Topper</a> at <a href="https://www.webman.io">webman.io</a>. Source available at <a href="https://github.com/marktopper/keygen.io">Github</a>. <span style="float: right;"></span><a href="//www.iubenda.com/privacy-policy/533112" class="iubenda-white iubenda-embed" title="Privacy Policy">Privacy Policy</a><script type="text/javascript">(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);</script></span>
 				</footer>
 			</div>
 		</div>
