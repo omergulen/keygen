3ccaa8e5c5c0dda6bd1982049fe0211cb767fffc
Andrei Ungur <andrei.ungur@mail.mcgill.ca>
2017-10-23 21:30:55 -0400

    Address other nitpick.ci comments
    Nitpick.ci doesn't like how my code looks, hopefully now it does.

--- global.php
+++ global.php
@@ -80,13 +80,10 @@ function generateRandomWEPKey($bits)
 function generateRandomSHAKey($bits)
 {
     $random_pass = random(15);
-    
-    if($bits == 256) {
-        
-        return hash('sha256',$random_pass);
-    } else if($bits == 128) {
-        
-        return hash('md5',$random_pass);
+    if ($bits == 256) {
+        return hash('sha256', $random_pass);
+    } else if ($bits == 128) {
+        return hash('md5', $random_pass);
     } else {
         return;
     }
